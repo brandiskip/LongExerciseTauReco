c317e548a4f4afdc43176b00b18491576232e244
brandiskip <bskipwo1@vols.utk.edu>
2021-07-16 17:08:59 +0200

    updated track and  reco loop

--- comtrackplotting.py
+++ comtrackplotting.py
@@ -34,19 +34,22 @@ histo_num = ROOT.TH1F('num', 'num', 20, 0, 20)
 ################################################################################################################################
 
 tch.Draw('tau_gen_lxy >> den', 'tau_gen_vis_pt>20 && abs(tau_gen_vis_eta)<2.1 && tau_gen_decaymode == 0')
-num_sel = ['tau_gen_lxy >> num', 'tau_gen_vis_pt>20 && abs(tau_gen_vis_eta)<2.1 && tau_gen_decaymode == 0 && tau_up_com_pt>0', 'tau_gen_lxy >> num', 'tau_gen_vis_pt>20 && abs(tau_gen_vis_eta)<2.1 && tau_gen_decaymode == 0 && tau_reco_pt>0']
+sel = ('tau_gen_lxy >> num')
+num_sel = ['tau_gen_vis_pt>20 && abs(tau_gen_vis_eta)<2.1 && tau_gen_decaymode == 0 && tau_up_com_pt>0', 'tau_gen_vis_pt>20 && abs(tau_gen_vis_eta)<2.1 && tau_gen_decaymode == 0 && tau_reco_pt>0']
 for nn in num_sel:
-	tch.Draw(nn)
+	tch.Draw(sel, nn)
+	print(nn)
+	c = ROOT.TCanvas("num")
+	c = ROOT.TCanvas("den")
+	c.cd()
+	eff = ROOT.TEfficiency(histo_num, histo_den)
+	eff.SetTitle('displaced #tau_{h}^{gen} ;transverse plane displacement (cm) ; efficiency')
+	eff.Draw('alpe')
 	
 
 ################################################################################################################################
-c = ROOT.TCanvas("num")
-c = ROOT.TCanvas("den")
-c.cd()
-eff = ROOT.TEfficiency(histo_num, histo_den)
 
-eff.SetTitle('displaced #tau_{h}^{gen} ;transverse plane displacement (cm) ; efficiency')
-eff.Draw('alpe')
+
 
 legend = ROOT.TLegend(.66, .64, .8, .88)
 legend.SetHeader("displaced #tau_{h}^{gen}")
